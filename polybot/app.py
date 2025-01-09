ca07b0fd1165dbe5ced2cb47eaa3b72070a28fb2
tals <tal.silbiger@harmonicinc.com>
2025-01-09 12:36:12 +0200

    changed file path to the secret

--- polybot/app.py
+++ polybot/app.py
@@ -5,7 +5,7 @@ from bot import ObjectDetectionBot
 
 app = flask.Flask(__name__)
 
-secrets_file_path = "/run/secrets/telegram_token"
+secrets_file_path = "/home/ubuntu/PolybotService/telegram_bot_token.secret"
 
 with open(secrets_file_path, "r") as file:
     TELEGRAM_TOKEN =  file.read().strip()
